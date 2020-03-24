3b3259eb8e7955456bbf85e5305f78b5af021653
tronquin <tronquinwantsmoney@gmail.com>
2020-03-23 23:08:50 -0400

    image optimization, webp on index page, minor layout changes

--- src/app/home/hello/hello.component.ts
+++ src/app/home/hello/hello.component.ts
@@ -9,6 +9,8 @@ export class HelloComponent implements OnInit {
   @Input() headline: string;
   @Input() description: string;
   @Input() image: string;
+  @Input() lambda: string;
+  showMyElement: boolean = false;
 
   constructor() {}
 
--- src/app/shared/pipe/SbConvertImage.pipe.ts
+++ src/app/shared/pipe/SbConvertImage.pipe.ts
@@ -3,18 +3,12 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "sbConvertImage" })
 export class SbConvertImage implements PipeTransform {
   transform(image: string, format: string): string {
-    const path: string = image.replace("https://a.storyblok.com", "");
+    const regex = new RegExp("(https):/{2}");
+    const path: string = regex.test(image)
+      ? image.replace("https://a.storyblok.com", "")
+      : image.replace("//a.storyblok.com", "");
     const imageService: string = "https://img2.storyblok.com/";
     const convertToWebp: string = `filters:format(${format})`;
     return `${imageService}${convertToWebp}${path}`;
   }
-
-  // supportsWebp(): Observable<any> {
-  //   const webpData =
-  //     "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=";
-  //   return from(fetch(webpData)).pipe(
-  //     flatMap(x => x.blob()),
-  //     flatMap(z => createImageBitmap(z))
-  //   );
-  // }
 }
