e810eb97ab209197b17bd70bcb46bc26554dbe69
tronquin <tronquinwantsmoney@gmail.com>
2020-05-14 05:13:42 -0400

    several visual changes and fixes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,38 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   open() {
-    const hamburger = document.querySelector('#ham');
-    const pageName = document.querySelector('#page-name');
-    const menu = document.querySelector('#menu');
-    const body = document.querySelector('body');
-    body.classList.toggle('no-scroll');
-    menu.classList.toggle('menu-open');
-    pageName.classList.toggle('open');
-    hamburger.classList.toggle('open');
+    const hamburger = document.querySelector("#ham");
+    const pageName = document.querySelector("#page-name");
+    const menu = document.querySelector("#menu");
+    const body = document.querySelector("body");
+    body.classList.toggle("no-scroll");
+    menu.classList.toggle("menu-open");
+    pageName.classList.toggle("open");
+    hamburger.classList.toggle("is-active");
   }
 
   close() {
-    const hamburger = document.querySelector('#ham');
-    const pageName = document.querySelector('#page-name');
-    const menu = document.querySelector('#menu');
-    const body = document.querySelector('body');
-    if(menu.classList.contains('menu-open')) {
-      body.classList.toggle('no-scroll');
+    const hamburger = document.querySelector("#ham");
+    const pageName = document.querySelector("#page-name");
+    const menu = document.querySelector("#menu");
+    const body = document.querySelector("body");
+    if (menu.classList.contains("menu-open")) {
+      body.classList.toggle("no-scroll");
     }
-    menu.classList.remove('menu-open');
-    pageName.classList.remove('open');
-    hamburger.classList.remove('open');
+    menu.classList.remove("menu-open");
+    pageName.classList.remove("open");
+    hamburger.classList.remove("open");
   }
 }
