17e584d009a8a469678a5298f0cb67f8e35a36d2
tronquin <tronquinwantsmoney@gmail.com>
2020-05-18 16:51:50 -0400

    subline added to about who am i

--- src/app/about/who-am-i/who-am-i.component.ts
+++ src/app/about/who-am-i/who-am-i.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-who-am-i',
-  templateUrl: './who-am-i.component.html',
-  styleUrls: ['./who-am-i.component.scss']
+  selector: "app-who-am-i",
+  templateUrl: "./who-am-i.component.html",
+  styleUrls: ["./who-am-i.component.scss"],
 })
 export class WhoAmIComponent implements OnInit {
-
   @Input() first_name: string;
   @Input() last_name: string;
   @Input() catch: string;
+  @Input() subline: string;
   @Input() brief: string;
   @Input() biography_1: string;
   @Input() biography_2: string;
   @Input() front: any;
   @Input() biography_image: any;
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
